b30874606f3cfbc6b5373fbc79ce67b3c8b1a019
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-09-29 18:58:24 -0500

    set state for todo

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { UserContext } from '../../context/UserContext';
 import './Todo.css';
@@ -6,23 +6,35 @@ import './Todo.css';
 
 export default function Todo() {
 
+  const [todo, setTodo] = useState('');
   const { user } = useContext(UserContext);
+
   if (!user) {
     return <Redirect to="/auth/sign-in" />;
   }
 
+  const handleClick = async () => {
+    // on click this function needs to run to send info to supabase 
+    // will also need to retrieve data to map out list of todos
+    // each todo will have a handlecomplete feature to mark as completed
+  };
+
   return (
-    <div className='todo'>
-      <h3>My To Do List:</h3>
+    <>
       <div className='todo-form'>
-        <input></input>
-        <button>Add</button>
+        <input value={todo} 
+          placeholder="Enter your task"
+          onChange={(e) => setTodo(e.target.value)}></input>
+        <button onClick={handleClick} >Add</button>
       </div>
       <div className='container'>
-        <p>Task 1</p>
-        <p>Task 2</p>
-        <p>Task 3</p>
+        <h3>My To Do List:</h3>
+        <div className='todo'>
+          <p>Task 1</p>
+          <p>Task 2</p>
+          <p>Task 3</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
