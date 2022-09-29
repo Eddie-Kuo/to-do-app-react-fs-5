49ce211a453a37291755b7ec275d6426000e3b30
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-09-29 18:09:53 -0500

    personalized header on todo page when user logs in. log out button added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,27 @@
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
+import { signOut } from '../../services/auth';
 import './Header.css';
 
 export default function Header() {
+  const { user, setUser } = useContext(UserContext);
+  // passing in user state with context
+
+  const handleClick = async () => {
+    await signOut();
+    setUser(null);
+  };
+
   return (
     <header className='header'>
       <h2>Alchemy To Do List</h2>
-
+      {user && (
+        <>
+          <div>Hello, {user.email}</div>
+          <button onClick={handleClick} >Logout</button>
+        </>
+      )}
+      {/* This will only show if the user is logged in */}
     </header>
   );
 }
