c6748db84ea7490877438b7b5e167e440cbcbff4
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-09-28 19:49:33 -0500

    planned out README + header component layout

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header>
+      <h2>Alchemy To Do List</h2>
+      <p>
+        hello, _____
+      </p>
+      <p>
+          Logout
+      </p>
+    </header>
+  );
+}
